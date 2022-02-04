7d1ec61671a3b805561290760aec2cdcbe6e964a
RogerCastillo877 <roger.castillo877@gmail.com>
2022-02-04 17:08:41 -0500

    firts dispatch

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,23 +1,46 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { login } from '../../actions/auth';
+import { useForm } from '../../hooks/UseForm';
 
 export const Login = () => {
+
+  const dispatch = useDispatch();
+
+  const [ formValues, handleInputChange ] = useForm({
+    email: 'correo@correo.com',
+    password: 1234
+  });
+
+  const { email, password } = formValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    dispatch( login(1234, 'Rocas') );
+  };
+
   return (
       <>
           <h3 className='auth__title'>Login</h3>
-          <form>
+          <form onSubmit={ handleLogin }>
             <input
               type="text"
               placeholder='email'
               name='email'
               className='auth__input'
               autoComplete='off'
+              value={ email }
+              onChange={ handleInputChange }
             />
             <input
               type="password"
               placeholder='password'
               name='password'
               className='auth__input'
+              value={ password }
+              onChange={ handleInputChange }
             />
             <button
               type='submit'
