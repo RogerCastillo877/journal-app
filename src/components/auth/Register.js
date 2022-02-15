05e7ab1004f3dab015fa954d2aae27709ee1e398
RogerCastillo877 <roger.castillo877@gmail.com>
2022-02-15 12:31:22 -0500

    uiReducer and actions

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import validator from 'validator';
+import { removeError, setError } from '../../actions/ui';
 import { useForm } from '../../hooks/UseForm'
 
 export const Register = () => {
+
+  const dispatch = useDispatch();
 
   const [ formValues, handleInputChange ] = useForm({
     name: 'Rocas',
@@ -25,16 +29,17 @@ export const Register = () => {
   const isFormValid = () => {
 
     if( name.trim().length === 0 ) {
-      console.log('Name is required');
+      dispatch(setError('Name is required'))
       return false;
     } else if( !validator.isEmail(email) ) {
-      console.log('Email is not valid');
+      dispatch(setError('Email is not valid'))
       return false;
     } else if( password !== password2 || password.length > 5 ) {
-      console.log('Password should be at least 6 characters and match each other');
+      dispatch(setError('Password should be at least 6 characters and match each other'))
       return false;
     }
 
+    dispatch( removeError() );
     return true;
   }
 
