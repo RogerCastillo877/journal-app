4a2c2570f63a6144c3add2f903c174f87286234b
RogerCastillo877 <roger.castillo877@gmail.com>
2022-02-16 08:39:59 -0500

    global loading

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { firebase } from '../firebase/firebase-config'
@@ -10,18 +10,31 @@ export const AppRouter = () => {
 
   const dispatch = useDispatch();
 
+  const [ checking, setChecking ] = useState(true);
+  const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+
   useEffect(() => {
     
     firebase.auth().onAuthStateChanged( (user) => {
       
       if( user?.uid ) {
         dispatch( login( user.uid, user.displayName ) )
+        setChecking( true );
+      } else {
+        setIsLoggedIn( false );
       }
+
+      setChecking(false);
 
     });
 
-  }, [])
+  }, [dispatch, setChecking, isLoggedIn])
   
+  if( checking ) {
+    return (
+      <h1>Aguarde un momento por favor...</h1>
+    )
+  }
 
   return (
     <Router>
