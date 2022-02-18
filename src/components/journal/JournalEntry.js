dfaae895ce4dd02123e4850d67d0c830acc6cb4a
RogerCastillo877 <roger.castillo877@gmail.com>
2022-02-18 17:53:39 -0500

    Activate note to edit

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,34 +1,40 @@
 import moment from 'moment';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 
 export const JournalEntry = ({ id, date, title, body, url }) => {
+  const noteDate = moment(date);
+  const dispacth = useDispatch();
 
-    const noteDate= moment(date);
+  const handleEntryClick = (id, uid) => {
+    dispacth(
+      activeNote(id, {
+        date, title, body, url
+      })
+    );
+  };
 
-    return (
-      <div className='journal__entry pointer'>
-        {
-            url &&
-            <div
-                className='journal__entry-picture'
-                style={{
-                    backgroundSize: 'cover',
-                    backgroundImage: `url(${ url })`
-                }}
-            ></div>
-        }
-        <div className='journal__entry-body'>
-            <p className='journal__entry-title'>
-                { title }
-            </p>
-            <p className='journal__entry-content'>
-                { body }
-            </p>
-        </div>
-        <div className='journal__entry-date-box'>
-            <span>{ noteDate.format('dddd') }</span>
-            <h4>{ noteDate.format('Do') }</h4>
-        </div>
+  return (
+    <div className="journal__entry pointer"
+    onClick={handleEntryClick}>
+      {url && (
+        <div
+          className="journal__entry-picture"
+          style={{
+            backgroundSize: 'cover',
+            backgroundImage: `url(${url})`,
+          }}
+        ></div>
+      )}
+      <div className="journal__entry-body">
+        <p className="journal__entry-title">{title}</p>
+        <p className="journal__entry-content">{body}</p>
       </div>
-  )
+      <div className="journal__entry-date-box">
+        <span>{noteDate.format('dddd')}</span>
+        <h4>{noteDate.format('Do')}</h4>
+      </div>
+    </div>
+  );
 };
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { NotesAppBar } from './NotesAppBar';
+import { useForm } from '../../hooks/UseForm';
 
 export const NoteScreen = () => {
+
+  const { active:note } = useSelector( state => state.notes );
+  const dispatch = useDispatch();
+  const [ formValues, hadleInputChange ] = useForm( note );
+
+  console.log(formValues);
+  const { body, title } = formValues;
+
   return (
       <div className='notes__main-content'>
           <NotesAppBar />
@@ -11,16 +21,23 @@ export const NoteScreen = () => {
               placeholder='Some awesome title'
               className='notes__title-input'
               autoComplete='off'
+              value={title}
+              onChange={ hadleInputChange }
             />
             <textarea
               placeholder='What happend today'
-              className='notes__textarea'>
+              className='notes__textarea'
+              value={body}
+              onChange={ hadleInputChange }>
             </textarea>
-            <div className='notes__image'>
-              <img
-                src=""
-                alt="ImageSelected" />
-            </div>
+            {
+              (note.url) &&
+              (<div className='notes__image'>
+                <img
+                  src=""
+                  alt="ImageSelected" />
+              </div>)
+            }
           </div>
       </div>
   )
