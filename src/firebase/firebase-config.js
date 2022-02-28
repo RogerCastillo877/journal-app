bb1150bb41908d2500166d74cf0dae079297282e
RogerCastillo877 <roger.castillo877@gmail.com>
2022-02-28 15:58:37 -0500

    Create a database for testing

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -9,9 +9,25 @@ const firebaseConfig = {
     storageBucket: "journal-app-react-71695.appspot.com",
     messagingSenderId: "20642473308",
     appId: "1:20642473308:web:38af3628089bb5e8874d99"
+}
+;
+const firebaseConfigTesting = {
+    apiKey: "AIzaSyCbWx6IXnz_9dXTHR_NBSdQ1C9r_oCdpaU",
+    authDomain: "dababase-test.firebaseapp.com",
+    projectId: "dababase-test",
+    storageBucket: "dababase-test.appspot.com",
+    messagingSenderId: "912570838167",
+    appId: "1:912570838167:web:75557860a72bd3dfadcdd2"
 };
 
-firebase.initializeApp(firebaseConfig);
+if( process.env.NODE_ENV === 'test' ){
+    //  Dev
+    firebase.initializeApp(firebaseConfigTesting);
+} else {
+    //  Prod
+    firebase.initializeApp(firebaseConfig);
+}
+
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
